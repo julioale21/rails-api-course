4fecded40f3ce94babf9751fad620c3480b0353c
julioale21 <30907777+julioale21@users.noreply.github.com>
2023-08-24 16:10:43 -0300

    added session controller and added bcrypt gem

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # devise_for :users
   namespace :v1 do
     resources :contacts
-    resources :session, only: [:create, :destroy]
+    resources :sessions, only: [:create, :destroy]
   end
 end
