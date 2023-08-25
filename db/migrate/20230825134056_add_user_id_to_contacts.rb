724ce4a9254774ced27c7854f02ab3557f78abbf
julioale21 <30907777+julioale21@users.noreply.github.com>
2023-08-25 10:52:44 -0300

    created relationship between user and contacts

---
+++ db/migrate/20230825134056_add_user_id_to_contacts.rb
@@ -0,0 +1,5 @@
+class AddUserIdToContacts < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :contacts, :user, index: true, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_23_235722) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_25_134056) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_235722) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_contacts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,8 +32,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_235722) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "authentication_token", limit: 30
+    t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "contacts", "users"
 end
