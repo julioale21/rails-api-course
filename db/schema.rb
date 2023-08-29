865bbecaac6b7a69a243146f6894d2aa2bd98934
julioale21 <30907777+julioale21@users.noreply.github.com>
2023-08-28 22:00:05 -0300

    updated schema with migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_28_232246) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_29_004800) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_28_232246) do
     t.bigint "owner_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "address"
+    t.float "var_rate"
+    t.string "tax_payer_id"
+    t.string "default_currency"
     t.index ["owner_id"], name: "index_accounts_on_owner_id"
     t.index ["slug"], name: "index_accounts_on_slug", unique: true
   end
